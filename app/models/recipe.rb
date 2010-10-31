25e7c752604db4b3f449bcf73d7b6b0bcf678614
Clifford Engle <cliffengle@gmail.com>
2010-10-31 15:22:39 -0700

    Create recipes with ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ActiveRecord::Base
   has_many :reviews, :dependent => :destroy
   has_many :ingredients, :dependent => :destroy
   belongs_to :recipe_list
+  accepts_nested_attributes_for :ingredients, :allow_destroy => true, :reject_if => :all_blank
 end
