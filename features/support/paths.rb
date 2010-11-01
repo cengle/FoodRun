74e73d23da037e81fc7c4c7fe39bc4b754fd9b31
Swaraj <swarajban@gmail.com>
2010-11-01 11:28:44 -0700

    Commiting paths file

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,6 +10,9 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
+      
+    when /the create recipe page/
+      '/recipes/new'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
