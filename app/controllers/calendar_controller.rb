0bc5f809a58bc71290af5cef18f86e2e55fe0840
Ai Jiang <flamingfurball89@yahoo.com>
2010-12-09 19:09:20 -0800

    calendar not working

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -6,8 +6,15 @@ class CalendarController < ApplicationController
 
     @shown_month = Date.civil(@year, @month)
 
-    @event_strips = Event.event_strips_for_month(@shown_month)
-      #
+    #@event_strips = Event.event_strips_for_month(@shown_month)
+	@event_strips = Event.event_strips_for_month(@shown_month, :conditions => 'name = "first_event"')
+	  start_d, end_d = Event.get_start_and_end_dates(@shown_month) # optionally pass in @first_day_of_week
+  @events = Event.events_for_date_range(start_d, end_d)
+  @events << Event.new({:name => "third event", :start_at => Time.now, :end_at => Time.now})
+  @event_strips = Event.create_event_strips(start_d, end_d, @events)
+
+
+	#
 
   end
   
