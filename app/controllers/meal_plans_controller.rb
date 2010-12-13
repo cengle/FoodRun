dbc90b3b6dc1aeab0cb672a9922bd656c3b77947
Swaraj <swarajban@gmail.com>
2010-12-13 02:12:13 -0800

    - Added ability to delete recipe from meal plan on both meal plan creation page and 'show' page - Tweaked auto_complete settings slightly

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -32,41 +32,51 @@ class MealPlansController < ApplicationController
 		format.xml {render :xml => @meal_plans }
 	end
   end
+  
 
   # GET /meal_plans/new
   # GET /meal_plans/new.xml
   def new
-    @meal_plan = MealPlan.new
-    @my_recipes = current_user.recipe_list.recipes
+    @meal_plan = MealPlan.find(params[:id])
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @meal_plan }
     end
+  end 
+  
+  def newHelper
+  	@meal_plan = MealPlan.new(:user_id => current_user.id, :name => "Default Meal Plan Name", 
+  					:description => "Enter Meal Plan Description")
+  	@meal_plan.user_id = current_user.id
+  	for recipe in @meal_plan.user.recipe_list.recipes
+    		@meal_plan.recipes << recipe
+    	end
+    	@meal_plan.number = @meal_plan.recipes.size
+    	if @meal_plan.save
+    		redirect_to :action => 'new', :id => @meal_plan.id
+    	else
+    		redirect_to :controller => 'calendar', :action => 'index'
+    	end
   end
 
   # GET /meal_plans/1/edit
   def edit
     @meal_plan = MealPlan.find(params[:id])
   end
-
+  
   # POST /meal_plans
   # POST /meal_plans.xml
   def create    
-    @meal_plan = MealPlan.new(params[:meal_plan])
-    @meal_plan.user_id = current_user.id
-    for recipe in @meal_plan.user.recipe_list.recipes
-    	@meal_plan.recipes << recipe
-    end
-    @meal_plan.number = @meal_plan.recipes.size
-
+    @meal_plan = MealPlan.find(params[:id])
+    
     respond_to do |format|
-      if @meal_plan.save
-        format.html { redirect_to(@meal_plan, :notice => 'MealPlan was successfully created.') }
-        format.xml  { render :xml => @meal_plan, :status => :created, :location => @meal_plan }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @meal_plan.errors, :status => :unprocessable_entity }
-      end
+    	if @meal_plan.update_attributes(params[:meal_plan])
+    		redirect_to(@meal_plan, :notice => 'MealPlan was successfully created.')
+    	else
+		format.html { render :controller => 'calendar', :action => 'index' }
+		format.xml  { render :xml => @meal_plan.errors, :status => :unprocessable_entity }
+		
+    	end
     end
   end
 
@@ -104,9 +114,28 @@ class MealPlansController < ApplicationController
 	if (@results.empty?)
 	  flash.now[:notice] = 'No results found.'
 	end
+	
 	respond_to do |format|
-      format.html # new.html.erb
-    end
+      		format.html # new.html.erb
+   	end
+  end
+  
+  def removeRecipe
+  	@meal_plan = MealPlan.find(params[:id])
+  	recipe_id = params[:recipe_id]
+  	@meal_plan.removeRecipe(recipe_id)
+  	@meal_plan.update_attributes(:number => @meal_plan.recipes.size)
+	redirect_to :action => 'new', :id => @meal_plan.id
+  end
+  
+  
+  def removeRecipeFromExisting
+  	meal_plan_id = params[:id]
+ 	recipe_id = params[:recipe_id]
+  	@meal_plan = MealPlan.find(params[:id])
+  	@meal_plan.removeRecipe(recipe_id)  		
+  	@meal_plan.update_attributes(:number => @meal_plan.recipes.size)
+  	redirect_to :action => 'show', :id => meal_plan_id
   end
     
   
--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,4 +1,7 @@
 class MealPlan < ActiveRecord::Base
 	has_and_belongs_to_many :recipes
 	belongs_to :user
+	def removeRecipe(recipe_to_delete)
+    		recipes.delete(recipes.find(recipe_to_delete))
+  	end
 end
