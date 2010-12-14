e9649468071090878d8f13e1acb87e548aa6149f
Clifford Engle <cliffengle@gmail.com>
2010-12-13 23:08:15 -0800

    Can populate ingredient table with common ingredients

--- app/controllers/ingredient_amounts_controller.rb
+++ app/controllers/ingredient_amounts_controller.rb
@@ -1,5 +1,19 @@
 class IngredientAmountsController < ApplicationController
 	def index
-		@ingredients = Ingredient.find(:all, :conditions => ['lower(name) LIKE ?', "#{params[:search].downcase}%"])
+          search_tokens = params[:search].downcase().split()
+          sql_query = 'lower(name) LIKE ?'
+          conditions = []
+          conditions[0] = ""
+          i = 1
+          search_tokens.each do |token|
+            conditions[0] << sql_query
+            conditions[i] = "%#{token}%"
+            conditions[0] << " AND "
+            i += 1
+          end
+          conditions[0] = conditions[0][0..-6] #Remove trailing AND
+          # @ingredients = Ingredient.find(:all, :conditions => ['lower(name) LIKE ?', "#{params[:search].downcase}%"])
+          @ingredients = Ingredient.find(:all, :conditions => conditions)
+          @ingredients.sort!{ |x,y| x.name.length <=> y.name.length}
 	end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,47 @@
 class Ingredient < ActiveRecord::Base
   has_many :ingredient_amounts
   has_many :recipes, :through => :ingredient_amounts
+  
+  def self.load_ingredient_database
+    self.load_ingredients("public/ingredients.txt")
+  end
+
+  def self.load_ingredients(filename)
+  File.open(filename).each do |line|
+    l = line.split('~')
+    code = l[1]
+    name = l[3].downcase().titleize()
+    section = nil
+    if (l[1] =~ /^01[0-9]*$/)#Dairy & Eggs
+      section = "Dairy"
+    elsif (l[1] =~ /^02[0-9]*$/ or
+           l[1] =~ /^04[0-9]*$/ or
+           l[1] =~ /^12[0-9]*$/)
+      section = "Ingredients"
+    elsif (l[1] =~ /^09[0-9]*$/ or 
+           l[1] =~ /^11[0-9]*$/ or
+           l[1] =~ /^16[0-9]*$/)#Fruits and Vegetables
+      section = "Produce"
+    elsif (l[1] =~ /^05[0-9]*$/ or
+           l[1] =~ /^07[0-9]*$/ or
+           l[1] =~ /^10[0-9]*$/ or
+           l[1] =~ /^13[0-9]*$/ or
+           l[1] =~ /^15[0-9]*$/ or
+           l[1] =~ /^17[0-9]*$/)
+      section = "Meat and Seafood"
+    elsif (l[1] =~ /^08[0-9]*$/ or
+           l[1] =~ /^18[0-9]*$/ or
+           l[1] =~ /^19[0-9]*$/ or
+           l[1] =~ /^20[0-9]*$/ or
+           l[1] =~ /^23[0-9]*$/)
+      section = "Bakery, Grains, and Pastas"
+    elsif (l[1] =~ /^35[0-9]*$/)
+      section = "International"
+    end
+      if (section != nil)
+        i = Ingredient.new(:name => name, :section => section)
+        i.save
+      end
+    end
+  end
 end
