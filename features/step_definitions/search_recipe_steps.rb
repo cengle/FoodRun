928faef102dab3b99c05bfc265d0a89591ba423c
Swaraj <swarajban@gmail.com>
2010-12-14 00:17:04 -0800

    Fixed improve search recipe Cucumber

--- features/step_definitions/search_recipe_steps.rb
+++ features/step_definitions/search_recipe_steps.rb
@@ -1,24 +1,13 @@
-When /^I fill in 'Search for recipe' field with Chicken Burrito$/ do
-	fill_in('input', :with => 'Chicken Burrito')
-end
-
 When /^I click Search$/ do
   	click_button('search-submit')
 end
 
 Then /^I should see the search results list$/ do
 	if response.respond_to? :should
-		response.should contain('Search results')
+		response.should contain('Recipe Search Results')
 	else
-		assert_contain 'Search results'
+		assert_contain 'Search Results'
 	end
 end
 
-Then /^I should see Chicken Burrito$/ do
-	if response.respond_to? :should
-		response.should contain('Chicken Burrito')
-	else
-		assert_contain 'Chicken Burrito'
-	end
-end
 
