9cd7b05c5fc3ed188b133d2b17356c999fdab5f1
Justin Chu <Justin@.(none)>
2010-12-13 21:50:24 -0800

    CSS Tabs

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -111,10 +111,10 @@ class RecipesController < ApplicationController
   
   def search
   	flash.discard(:notice)
-	@results = Recipe.find(:all, :conditions => ['title LIKE ?', "%#{params[:input]}%"])
+	@recipe_results = Recipe.find(:all, :conditions => ['title LIKE ?', "%#{params[:input]}%"])
 	
 	@input = params[:input]
-	if (@results.empty?)
+	if (@recipe_results.empty?)
 	  flash.now[:notice] = 'No results found.'
 	end
 	respond_to do |format|
