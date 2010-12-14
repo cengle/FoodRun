919b1ff595bf2381031d70238379ce057b9c1a91
Swaraj <swarajban@gmail.com>
2010-12-14 00:20:18 -0800

    Fixed minor cucumber errors

--- features/step_definitions/grocery_list_view_steps.rb
+++ features/step_definitions/grocery_list_view_steps.rb
@@ -1,12 +1,3 @@
-=begin
-Given /^I have added a cookie recipe to the recipe list named "([^"]*)"$/ do |recipe_name|
-  visit '/recipes/new'
-  fill_in(:title, :with => recipe_name)
-  fill_in(:description, :with => 'blah blah blah description')
-  click_link('Upload Recipe')
-end
-
-=end
 Given /^I have added two recipes that have (\d+) and (\d+) units of ingredient test$/ do |arg1, arg2|
 	r1 = Recipe.new(:title => 'Test Recipe 1', 
 		:description => 'This is a test recipe description')
@@ -21,4 +12,3 @@ Given /^I have added two recipes that have (\d+) and (\d+) units of ingredient t
 end
 
 
->>>>>>> 20ff8e86300f0cc0f10035c8afe95c22bea2f2d5
