7047523a41af4c932102ff9da0e3b12a5e030f97
Swaraj <swarajban@gmail.com>
2010-12-13 16:34:22 -0800

    All ingredient names now titleized

--- app/models/ingredient_amount.rb
+++ app/models/ingredient_amount.rb
@@ -2,12 +2,12 @@ class IngredientAmount < ActiveRecord::Base
   belongs_to :ingredient
   belongs_to :recipe
   def ingredient_name= name	
-    i = Ingredient.find_by_name(name.downcase())
+    i = Ingredient.find_by_name(name.downcase().titleize())
 	if i
 		self.ingredient_id = i.id
 	else
 		i = Ingredient.create
-		i.name = name.downcase()
+		i.name = name.downcase().titleize()
 		i.save
 		self.ingredient_id = i.id
 	end
